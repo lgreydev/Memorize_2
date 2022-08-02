48fb810ffe12d29dd42fe6c0caccbafeb471eea0
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-08-02 15:56:21 +0300

    Initial Commit

---
+++ Memorize_2/Memorize_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_2App.swift
+//  Memorize_2
+//
+//  Created by Sergey Lukaschuk on 02.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
