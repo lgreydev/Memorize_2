0eab77235d0199054cb85544cdf2d3f9a7ad54a6
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-08-18 16:32:46 +0300

    Optional index

--- Memorize_2/MemoryGame.swift
+++ Memorize_2/MemoryGame.swift
@@ -20,17 +20,18 @@ struct MemoryGame<CardContent> {
     }
 
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
 
-    private func index(of card: Card) -> Int {
+    private func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0
+        return nil
     }
 
     struct Card: Identifiable {
