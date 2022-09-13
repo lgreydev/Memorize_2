14e68d925521ce22e946a8146bf3787bbc8bb766
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-13 05:58:26 +0300

    Fix modifier

--- Memorize_2/View/CardView.swift
+++ Memorize_2/View/CardView.swift
@@ -16,21 +16,13 @@ struct CardView: View {
 
     var body: some View {
         GeometryReader { geometry in
-            let shape = RoundedRectangle(cornerRadius: Constants.cornerRadius)
             ZStack() {
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: Constants.lineWidth)
-                    PieView(starAngel: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
-                        .padding(5)
-                        .opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0.0)
-                } else {
-                    shape.fill()
-                }
+                PieView(starAngel: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+                    .padding(5)
+                    .opacity(0.5)
+                Text(card.content).font(font(in: geometry.size))
             }
+            .modifier(Cardify(isFaceUp: card.isFaceUp))
         }
     }
 
@@ -39,8 +31,6 @@ struct CardView: View {
     }
 
     private struct Constants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.6
     }
 }
--- Memorize_2/View/Cardify.swift
+++ Memorize_2/View/Cardify.swift
@@ -17,6 +17,7 @@ struct Cardify: ViewModifier {
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: Constants.lineWidth)
+                content
             } else {
                 shape.fill()
             }
@@ -26,5 +27,11 @@ struct Cardify: ViewModifier {
     private struct Constants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
     }
 }
